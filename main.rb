ca88fdba0e74e540d6db416de4d296e2e418ec3e
TheIncredibleHarsh <harsh.singh343@gmail.com>
2025-01-13 17:59:27 +0530

    add readme and main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
-p "Hello World"
+require_relative 'lib/string_calculator.rb'
+
+def get_input
+    all_text = ""
+    while (text = gets) != "\n"
+        all_text << text
+    end
+    all_text
+end
+
+all_text = get_input
+puts StringCalculator.calculate(all_text)
---
+++ readme.txt
@@ -0,0 +1,9 @@
+To run tests:
+- bundle install
+- rspec
+
+
+To run the calculator program:
+- ruby main.rb
+- Enter the input string with or without new lines, enter 2 consecutive new lines to end the input
+- The output will be the sum of all the numbers
