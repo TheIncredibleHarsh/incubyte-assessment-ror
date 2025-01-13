0c24927fd2f5e2f457b0c62c8e2c46ffa7c0070f
TheIncredibleHarsh <harsh.singh343@gmail.com>
2025-01-13 15:09:27 +0530

    Handle single digits

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
     def self.calculate(string)
-        return 0
+        return 0 if string.empty?
+        string.to_i
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ describe StringCalculator do
     it "should return 0 for empty string" do
         expect(StringCalculator.calculate("")).to eq(0)
     end
+
+    it "should return the number itself for a single number" do
+        expect(StringCalculator.calculate("1")).to eq(1)
+    end
 end
